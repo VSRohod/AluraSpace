932494a7623719acd1bd98390f225c8354b39b49
Victor Santos Rohod <victorsantosrohod@gmail.com>
2024-03-12 15:36:27 -0300

    Update

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from galeria.models import Fotografia
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = Fotografia.objects.all()    
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
